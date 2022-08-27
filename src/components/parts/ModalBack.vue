9fd81685cab37c7dcdcd0e42a2d0be8576396f08
mirumi <mail@mirumi.tech>
2022-08-27 17:02:48 +0900

    fix: transition properties

--- src/components/parts/ModalBack.vue
+++ src/components/parts/ModalBack.vue
@@ -8,10 +8,7 @@
 <style lang="scss" scoped>
 .modal_back {
   position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  inset: 0;
   display: block;
   background-color: #00000080;
   cursor: default;
