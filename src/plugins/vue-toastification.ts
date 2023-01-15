1770b6f2bcfebcf82a6ee8ca5a4757e9305416bc
mirumi <mail@mirumi.tech>
2023-01-15 23:05:21 +0900

    add: toast plugin to support Nuxt 3

---
+++ src/plugins/vue-toastification.ts
@@ -0,0 +1,27 @@
+import Toast, { POSITION } from "vue-toastification"
+import type { PluginOptions } from "vue-toastification"
+import "vue-toastification/dist/index.css"
+import "@/assets/styles/toast.scss"
+
+const toastOptions: PluginOptions = {
+  closeButton: "button",
+  closeOnClick: false,
+  draggable: false,
+  draggablePercent: 0.6,
+  hideProgressBar: true,
+  icon: false,
+  maxToasts: 7,
+  newestOnTop: true,
+  pauseOnFocusLoss: false,
+  pauseOnHover: true,
+  position: POSITION.BOTTOM_CENTER,
+  rtl: false,
+  showCloseButtonOnHover: true,
+  timeout: 4444,
+  // timeout: false,
+  transition: "original_fade",
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Toast, toastOptions)
+})
